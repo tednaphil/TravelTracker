f0bcfaf41347acb13f08876d0dbde75fa2354dc6
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-27 20:50:39 -0600

    Add test trip data

---
+++ test/sample-data/sample-trips.js
@@ -0,0 +1,96 @@
+const testTrips = [
+    { 
+        date: "2023/02/14", 
+        destinationID: 48, 
+        duration: 6, 
+        id: 1, 
+        status: "approved", 
+        suggestedActivities: [], 
+        travelers: 1, 
+        userID: 1 
+      },
+      { 
+        date: "2023/03/20", 
+        destinationID: 53, 
+        duration: 5, 
+        id: 2, 
+        status: "approved", 
+        suggestedActivities: [], 
+        travelers: 2, 
+        userID: 2 
+      },
+      { 
+        date: "2023/04/02", 
+        destinationID: 49, 
+        duration: 3, 
+        id: 3, 
+        status: "approved", 
+        suggestedActivities: [], 
+        travelers: 1, 
+        userID: 3 
+      },
+      { 
+        date: "2025/01/10", 
+        destinationID: 50, 
+        duration: 4, 
+        id: 4, 
+        status: "pending", 
+        suggestedActivities: [], 
+        travelers: 2, 
+        userID: 1 
+      },
+      { 
+        date: "2025/02/18", 
+        destinationID: 51, 
+        duration: 7, 
+        id: 5, 
+        status: "pending", 
+        suggestedActivities: [], 
+        travelers: 3, 
+        userID: 2 
+      },
+      { 
+        date: "2025/03/25", 
+        destinationID: 52, 
+        duration: 5, 
+        id: 6, 
+        status: "pending", 
+        suggestedActivities: [], 
+        travelers: 1, 
+        userID: 3 
+      },
+      { 
+        date: "2026/04/10", 
+        destinationID: 53, 
+        duration: 4, 
+        id: 7, 
+        status: "approved", 
+        suggestedActivities: [], 
+        travelers: 2, 
+        userID: 1 
+      },
+      { 
+        date: "2026/05/18", 
+        destinationID: 48, 
+        duration: 6, 
+        id: 8, 
+        status: "approved", 
+        suggestedActivities: [], 
+        travelers: 3, 
+        userID: 2 
+      },
+      { 
+        date: "2026/06/25", 
+        destinationID: 49, 
+        duration: 5, 
+        id: 9, 
+        status: "approved", 
+        suggestedActivities: [], 
+        travelers: 1, 
+        userID: 3 
+      }
+];
+
+export {
+    testTrips
+}
