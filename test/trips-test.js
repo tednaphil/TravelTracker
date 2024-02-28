4b1b1f300afbfe0287e84664461f280cb053eca2
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-28 17:12:59 -0600

    Import data and functions

--- test/trips-test.js
+++ test/trips-test.js
@@ -1,9 +1,25 @@
 import chai from 'chai';
 const expect = chai.expect;
 import { testTrips } from './sample-data/sample-trips';
+import { filterTrips } from '../src/trips';
+import { testTravelers } from './sample-data/sample-traveler';
+import { setTraveler } from '../src/traveler';
 
-describe('See if trips tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
+describe('Trips', function() {
+  // it('should return true', function() {
+  //   expect(true).to.equal(true);
+  // });
+  let traveler1, traveler2;
+  beforeEach(() => {
+    traveler1 = setTraveler(1, testTravelers);
+    traveler2 = setTraveler(2, testTravelers);
+  })
+  console.log(traveler1)//why undefined? check imports
+  
+  describe('Filter Trips', function() {
+    it.skip('should return an array of one traveler\'s trips')
+    // const trips1 = filterTrips(traveler1.id, testTrips);
+
+    // expect(trips1.length).to.equal(3)
+  })
 });
