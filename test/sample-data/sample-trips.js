6c559be016ea10e382c15b0df2112102a8d73347
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-27 21:03:20 -0600

    Update test data

--- test/sample-data/sample-trips.js
+++ test/sample-data/sample-trips.js
@@ -1,7 +1,7 @@
 const testTrips = [
     { 
         date: "2023/02/14", 
-        destinationID: 48, 
+        destinationID: 1, 
         duration: 6, 
         id: 1, 
         status: "approved", 
@@ -11,7 +11,7 @@ const testTrips = [
       },
       { 
         date: "2023/03/20", 
-        destinationID: 53, 
+        destinationID: 2, 
         duration: 5, 
         id: 2, 
         status: "approved", 
@@ -21,7 +21,7 @@ const testTrips = [
       },
       { 
         date: "2023/04/02", 
-        destinationID: 49, 
+        destinationID: 3, 
         duration: 3, 
         id: 3, 
         status: "approved", 
@@ -31,7 +31,7 @@ const testTrips = [
       },
       { 
         date: "2025/01/10", 
-        destinationID: 50, 
+        destinationID: 4, 
         duration: 4, 
         id: 4, 
         status: "pending", 
@@ -41,7 +41,7 @@ const testTrips = [
       },
       { 
         date: "2025/02/18", 
-        destinationID: 51, 
+        destinationID: 5, 
         duration: 7, 
         id: 5, 
         status: "pending", 
@@ -51,7 +51,7 @@ const testTrips = [
       },
       { 
         date: "2025/03/25", 
-        destinationID: 52, 
+        destinationID: 6, 
         duration: 5, 
         id: 6, 
         status: "pending", 
@@ -61,7 +61,7 @@ const testTrips = [
       },
       { 
         date: "2026/04/10", 
-        destinationID: 53, 
+        destinationID: 7, 
         duration: 4, 
         id: 7, 
         status: "approved", 
@@ -71,7 +71,7 @@ const testTrips = [
       },
       { 
         date: "2026/05/18", 
-        destinationID: 48, 
+        destinationID: 8, 
         duration: 6, 
         id: 8, 
         status: "approved", 
@@ -81,7 +81,7 @@ const testTrips = [
       },
       { 
         date: "2026/06/25", 
-        destinationID: 49, 
+        destinationID: 9, 
         duration: 5, 
         id: 9, 
         status: "approved", 
