bdb4013486247b7763af269eff8cd0c2e0b60345
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-27 19:57:12 -0600

    Add test suite and functionality files

---
+++ test/trips-test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('See if trips tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+});
