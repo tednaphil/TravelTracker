863870ee1eb71fb00f045b804e0135cc662b8991
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 11:07:04 -0600

    Add return to calculateTripCost

--- src/trips.js
+++ src/trips.js
@@ -19,8 +19,23 @@ function organizeTrips(userTrips) {
 }
 
 function calculateTripCost(tripID, tripsArray, destinationsArray) {
-
-
+    const foundTrip = tripsArray.find(trip => trip.id === tripID);
+    // console.log('trip', foundTrip)
+    const tripDest = destinationsArray.find(dest => foundTrip.destinationID === dest.id);
+    // console.log('destination', tripDest)
+    const totalLodging = tripDest.estimatedLodgingCostPerDay * foundTrip.duration;
+    const totalAirfare = tripDest.estimatedFlightCostPerPerson * foundTrip.travelers;
+    const subtotal = totalLodging + totalAirfare;
+    const agentFee = subtotal * .1;
+    const grandTotal = subtotal + agentFee
+    const tripCost = {
+        totalLodging,
+        totalAirfare,
+        subtotal,
+        agentFee,
+        grandTotal
+    }
+    return tripCost
 }
 
 // function createTrip() {
