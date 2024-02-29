02fcf068f7af7c812019df8537d8c9a6d4b05af0
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 11:19:15 -0600

    Refactor calculateTripCost

--- src/trips.js
+++ src/trips.js
@@ -19,23 +19,26 @@ function organizeTrips(userTrips) {
 }
 
 function calculateTripCost(tripID, tripsArray, destinationsArray) {
-    const foundTrip = tripsArray.find(trip => trip.id === tripID);
-    // console.log('trip', foundTrip)
-    const tripDest = destinationsArray.find(dest => foundTrip.destinationID === dest.id);
-    // console.log('destination', tripDest)
-    const totalLodging = tripDest.estimatedLodgingCostPerDay * foundTrip.duration;
-    const totalAirfare = tripDest.estimatedFlightCostPerPerson * foundTrip.travelers;
-    const subtotal = totalLodging + totalAirfare;
-    const agentFee = subtotal * .1;
-    const grandTotal = Math.round(subtotal + agentFee)
-    const tripCost = {
-        totalLodging,
-        totalAirfare,
-        subtotal,
-        agentFee,
-        grandTotal
+    const tripIDs = tripsArray.map(trip => trip.id);
+    if (!tripIDs.includes(tripID)) {
+        return false
+    } else {
+        const foundTrip = tripsArray.find(trip => trip.id === tripID);
+        const tripDest = destinationsArray.find(dest => foundTrip.destinationID === dest.id);
+        const totalLodging = tripDest.estimatedLodgingCostPerDay * foundTrip.duration;
+        const totalAirfare = tripDest.estimatedFlightCostPerPerson * foundTrip.travelers;
+        const subtotal = totalLodging + totalAirfare;
+        const agentFee = subtotal * .1;
+        const grandTotal = Math.round(subtotal + agentFee)
+        const tripCost = {
+            totalLodging,
+            totalAirfare,
+            subtotal,
+            agentFee,
+            grandTotal
+        }
+        return tripCost
     }
-    return tripCost
 }
 
 // function createTrip() {
