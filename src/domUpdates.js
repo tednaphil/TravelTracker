2337d1e50219b1c489b25ad46871ae3527fc4875
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 16:10:27 -0600

    Import functions and apiCalls

---
+++ src/domUpdates.js
@@ -0,0 +1,7 @@
+import { setTraveler, checkLogin } from "./traveler";
+import { filterTrips, organizeTrips, calculateTripCost, calculateStats, getTripDisplayInfo, createTrip } from "./trips";
+import { findDestination, getDestDisplayInfo, filterDestinations } from "./destinations";
+import { fetchData, postData } from "./apiCalls";
+
+
+
