d3c17f94c7c7dd50f35a02a21e767a78908c2d28
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-28 20:56:43 -0600

    Add return to organizeTrips

--- src/trips.js
+++ src/trips.js
@@ -4,7 +4,18 @@ function filterTrips(id, tripsArray) {
 }
 
 function organizeTrips(userTrips) {
-    
+    const tripsObject = userTrips.reduce((obj, trip) => {
+        if (trip.status === "approved") {
+            obj.approved.push(trip)
+        } else {
+            obj.pending.push(trip)
+        }
+        return obj
+    }, {
+        approved: [],
+        pending: []
+    })
+    return tripsObject
 }
 
 export {
