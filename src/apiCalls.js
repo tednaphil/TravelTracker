41c93623548b0e99b8f89f5987e363b324745edf
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 16:10:43 -0600

    Export apiCalls

---
+++ src/apiCalls.js
@@ -0,0 +1,18 @@
+const allData = [
+    fetch('http://localhost:3001/api/v1/travelers'),
+    fetch('http://localhost:3001/api/v1/trips'),
+    fetch('http://localhost:3001/api/v1/destinations')
+];
+
+function fetchData() {
+
+};
+
+function postData() {
+
+};
+
+export {
+    fetchData,
+    postData
+}
