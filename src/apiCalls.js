750b7d687f1b354756ac33bc44b00b792e0542df
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 16:57:36 -0600

    Add GET requests for travelers, trips, and destinations

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,42 @@
 const allData = [
-    fetch('http://localhost:3001/api/v1/travelers'),
+    fetch('http://localhost:3001/api/v1/travelers/'),
     fetch('http://localhost:3001/api/v1/trips'),
     fetch('http://localhost:3001/api/v1/destinations')
 ];
 
 function fetchData() {
-
+    return Promise.all(allData)
+    .then(responses => {
+        if (responses.every(response => response.ok)) {
+            return responses
+        } else {
+            let responseText = responses.find(response => !response.ok).statusText
+            let responseCode = responses.find(response => !response.ok).status
+            console.log('API GET error')
+            throw new Error(`${responseCode} : ${responseText}`)
+        }
+    })
+    .then(res => {
+        return Promise.all(res.map(obj => obj.json()))
+    })
+    .then(arr => {
+        // console.log(arr)
+        let dataObject = arr.reduce((obj, set) => {
+            let key = Object.keys(set)[0]
+            obj[key] = set[key]
+            return obj
+        }, {})
+        return dataObject
+    })
+    // .then(res => {
+    //     let keys = Object.keys(res)
+    //     console.log(keys)
+    //     console.log(res)
+    // })
+    .catch(error => {
+        console.log('API GET error caught')
+        throw new Error(error.message)
+    })
 };
 
 function postData() {
