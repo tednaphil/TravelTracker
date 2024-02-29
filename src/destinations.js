ce665fe4cf8fa2d801d9ac7704e24927bcfc0148
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 11:37:14 -0600

    Add return to findDestination

--- src/destinations.js
+++ src/destinations.js
@@ -1,22 +1,30 @@
 function findDestination(locationID, destinationsArray) {
-
+    const destinationIDs = destinationsArray.map(dest => dest.id);
+    if (!destinationIDs.includes(locationID)) {
+        return false
+    } else {
+        const destination = destinationsArray.find(dest => dest.id === locationID)
+        return destination
+    }
 }
+//refactor to destructure locationID parameter to {destinationID} and pass trip object instead?
+
 
 // function getDestinationCosts(locationID, destinationsArray) {
+        //MIGHT BE UNNECCESARY
+// }
 
+// function getDestinationDisplayInfo(destination) {
+    // return an object with image src, alt tag, and destination name
 // }
 
 // function filterDestinations() {
-
-// }
-
-// function addDestinationInfo() {
-
+    // return array of destinations that meet search criteria submitted by user
 // }
 
 export {
     findDestination,
     // getDestinationCosts,
-    // filterDestinations,
-    // addDestinationInfo
+    // addDestinationDisplayInfo,
+    // filterDestinations
 }
