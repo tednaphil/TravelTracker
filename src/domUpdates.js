c8e506bddc68b1cc4139ae8a979ae85c0812736e
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 17:31:07 -0600

    Add main function to set data for Dom updates

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,28 +6,38 @@ import { fetchData, postData } from "./apiCalls";
 
 // console.log(fetchData())
 
-// let travelersData;
+let travelersData;
 let tripsData;
 let destinationsData;
 // let userID;
 let currentTraveler;
 
-window.addEventListener('load', renderDom)
+window.addEventListener('load', fetchData()
+.then(({travelers, destinations, trips}) => {
+    travelersData =travelers;
+    tripsData = trips;
+    destinationsData = destinations;
+    renderDom()
+})
+.catch(error => {
+    console.log(error)
+    return error
+}))
+
+
 
 function renderDom() {
-    fetchData()
-        .then(({travelers, destinations, trips}) => {
-            currentTraveler = setTraveler(1, travelers);
-            // console.log(currentTraveler)
-            trips = tripsData
-            destinationsData = destinations
-        })
-        .catch(error => {
-            console.log(error)
-            return error
-        })
+    currentTraveler = setTraveler(1, travelersData);
+    console.log('currentTraveler', currentTraveler)
+    let trips = filterTrips(currentTraveler, tripsData);
+    console.log('trips', trips);
+    let organizedTrips = organizeTrips(trips);
+    console.log('organizedTrips', organizedTrips);
+    let tripDisplayDetails = getTripDisplayInfo(organizedTrips, destinationsData);
+    console.log('Trip Display Info', tripDisplayDetails)
+    //get totals next
+
 }
 
-console.log(currentTraveler)
-console.log(tripsData)
+
 
