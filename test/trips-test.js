e09ac0920ed7e412e5f1278ae0c6d4a30590f431
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-28 20:37:38 -0600

    Update beforeEach hook and add organizeTrips describe block

--- test/trips-test.js
+++ test/trips-test.js
@@ -1,20 +1,21 @@
 import chai from 'chai';
 const expect = chai.expect;
 import { testTrips } from './sample-data/sample-trips';
-import { filterTrips } from '../src/trips';
+import { filterTrips, organizeTrips } from '../src/trips';
 import { testTravelers } from './sample-data/sample-traveler';
 import { setTraveler } from '../src/traveler';
 
 describe('Trips', function() {
-  let traveler1, traveler2;
+  let traveler1, traveler2, trips1;
   beforeEach(() => {
     traveler1 = setTraveler(1, testTravelers);
     traveler2 = setTraveler(2, testTravelers);
+    trips1 = filterTrips(traveler1.id, testTrips);
   });
   
   describe('Filter Trips', function() {
     it('should return an array of one traveler\'s trips', function() {
-      const trips1 = filterTrips(traveler1.id, testTrips);
+      // const trips1 = filterTrips(traveler1.id, testTrips);
 
       expect(trips1).to.be.an('array')
       expect(trips1.length).to.equal(3);
@@ -44,8 +45,11 @@ describe('Trips', function() {
         travelers: 2, 
         userID: 2 
       });
+    })
+  })
 
-
+  describe('Organize Trips', function() {
+    it.skip('should create an object of a traveler\'s pending and approved trips', function() {
     })
   })
 });
