7eb107d8b15c08b30eea9092ca29a6cff9f3e3e5
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-29 16:58:04 -0600

    Add fetchData invokation

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -4,4 +4,30 @@ import { findDestination, getDestDisplayInfo, filterDestinations } from "./desti
 import { fetchData, postData } from "./apiCalls";
 
 
+// console.log(fetchData())
+
+// let travelersData;
+let tripsData;
+let destinationsData;
+// let userID;
+let currentTraveler;
+
+window.addEventListener('load', renderDom)
+
+function renderDom() {
+    fetchData()
+        .then(({travelers, destinations, trips}) => {
+            currentTraveler = setTraveler(1, travelers);
+            // console.log(currentTraveler)
+            trips = tripsData
+            destinationsData = destinations
+        })
+        .catch(error => {
+            console.log(error)
+            return error
+        })
+}
+
+console.log(currentTraveler)
+console.log(tripsData)
 
