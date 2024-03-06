c3dd05dd061a55089bf3ddf43b340744bc559b22
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-03-05 21:38:26 -0600

    Updates

--- bundle.js
+++ bundle.js
@@ -690,6 +690,8 @@ function displayNewTrip(tripObj) {
 
 function clearForm() {
   planTripForm.reset();
+  searchButton.disabled = true;
+  searchButton.classList.add('greyout');
 }
 
 function displayResults(e) {
